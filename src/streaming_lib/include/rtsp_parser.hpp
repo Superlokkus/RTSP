b746748fe99390079c12967d6896c94b4fd8a0f4
Markus Klemm <markus@markus@markusklemm.net>
2018-09-06 18:37:40 +0200

    Now with real number parsing for rtsp responses

--- src/streaming_lib/include/rtsp_parser.hpp
+++ src/streaming_lib/include/rtsp_parser.hpp
@@ -6,25 +6,27 @@
 #define RTSP_GUI_RTSP_PARSER_HPP
 
 #include <string>
+#include <cstdint>
 
 #include <boost/spirit/include/qi.hpp>
+
 
 namespace rtsp {
     struct request {
     };
     struct response {
-        std::string rtsp_version_major;
-        std::string rtsp_version_minor;
-        std::string status_code;
+        uint_fast16_t rtsp_version_major;
+        uint_fast16_t rtsp_version_minor;
+        uint_fast16_t status_code;
         std::string reason_phrase;
     };
     using message = boost::variant<request, response>;
 }
 BOOST_FUSION_ADAPT_STRUCT(
         rtsp::response,
-        (std::string, rtsp_version_major)
-        (std::string, rtsp_version_minor)
-        (std::string, status_code)
+        (uint_fast16_t, rtsp_version_major)
+        (uint_fast16_t, rtsp_version_minor)
+        (uint_fast16_t, status_code)
         (std::string, reason_phrase)
 )
 namespace rtsp {
@@ -34,20 +36,26 @@ namespace rtsp {
             : ::boost::spirit::qi::grammar<Iterator, response()> {
         rtsp_response_grammar() : rtsp_response_grammar::base_type(start) {
             namespace ns = ::boost::spirit::standard;
-            using ::boost::spirit::qi::uint_;
+            using ::boost::spirit::qi::uint_parser;
             using ::boost::spirit::qi::lexeme;
             using ::boost::spirit::qi::lit;
             using boost::spirit::qi::omit;
             using ::boost::spirit::qi::repeat;
             quoted_string %= lexeme['"' >> +(ns::char_ - '"') >> '"'];
-            start %= lit("RTSP/") >> +ns::digit >> "." >> +ns::digit >> omit[+ns::space]
-                    >> repeat(3)[ns::digit] >> omit[+ns::space]
+            status_code = uint_parser<uint_fast16_t, 10, 3, 3>();
+            at_least_one_digit = uint_parser<uint_fast16_t, 10, 1>();
+
+            start %= lit("RTSP/") >> at_least_one_digit >> "." >> at_least_one_digit >> omit[+ns::space]
+                    >> status_code >> omit[+ns::space]
                     >> *(ns::char_ - (lit("\r") | lit("\n")))
                     >> lit("\r\n");
         }
 
         boost::spirit::qi::rule<Iterator, std::string()> quoted_string;
+        boost::spirit::qi::rule<Iterator, uint_fast16_t()> status_code;
+        boost::spirit::qi::rule<Iterator, uint_fast16_t()> at_least_one_digit;
         boost::spirit::qi::rule<Iterator, response()> start;
+
     };
 }
 
--- src/streaming_lib/test/streaming_lib_tests.cpp
+++ src/streaming_lib/test/streaming_lib_tests.cpp
@@ -15,6 +15,7 @@ BOOST_AUTO_TEST_SUITE(rtsp)
             std::string ok_response{"RTSP/1.0\t200 \t  OK"};
             std::string ok_response_with_crlf{"RTSP/1.0\t200 \t  OK\r\n"};
             std::string ok_rtsp2_3_repsonse{"RTSP/2.3 200\tOK\r\n"};
+            std::string ok_rtsp25_19_repsonse{"RTSP/25.19 200\tOK\r\n"};
             std::string ok_http_1_1_repsonse{"HTTP/1.1 200  OK\r\n"};
             std::string ok_rtsp1_1_repsonse{"RTSP/1.1 200  OK\r\n"};
             std::string pause_request{
@@ -24,7 +25,11 @@ BOOST_AUTO_TEST_SUITE(rtsp)
                     {"\n\r fajfajkj \n\n\r\r\n"},
                     {"\r\n afsfas3244afs"},
                     {" \r\n"},
-                    {"jjlfsjflsjkl"}
+                    {"jjlfsjflsjkl"},
+                    {"RTSP/1.0\t2000 \t  OK\r\n"},
+                    {"RTSP/1.0\t20 \t  OK\r\n"},
+                    {"RTSP/.0\t200 \t  OK\r\n"},
+                    {"RTSP/ \t200 \t  OK\r\n"}
             };
             std::string::const_iterator begin{};
             std::string::const_iterator end{};
@@ -49,9 +54,9 @@ BOOST_AUTO_TEST_SUITE(rtsp)
                 parse_phrase(ok_response_with_crlf);
                 BOOST_CHECK(success);
                 BOOST_CHECK(begin == end);
-                BOOST_CHECK_EQUAL(response.rtsp_version_major, "1");
-                BOOST_CHECK_EQUAL(response.rtsp_version_minor, "0");
-                BOOST_CHECK_EQUAL(response.status_code, "200");
+                BOOST_CHECK_EQUAL(response.rtsp_version_major, 1);
+                BOOST_CHECK_EQUAL(response.rtsp_version_minor, 0);
+                BOOST_CHECK_EQUAL(response.status_code, 200);
                 BOOST_CHECK_EQUAL(response.reason_phrase, "OK");
             }
 
@@ -59,9 +64,19 @@ BOOST_AUTO_TEST_SUITE(rtsp)
                 parse_phrase(ok_rtsp2_3_repsonse);
                 BOOST_CHECK(success);
                 BOOST_CHECK(begin == end);
-                BOOST_CHECK_EQUAL(response.rtsp_version_major, "2");
-                BOOST_CHECK_EQUAL(response.rtsp_version_minor, "3");
-                BOOST_CHECK_EQUAL(response.status_code, "200");
+                BOOST_CHECK_EQUAL(response.rtsp_version_major, 2);
+                BOOST_CHECK_EQUAL(response.rtsp_version_minor, 3);
+                BOOST_CHECK_EQUAL(response.status_code, 200);
+                BOOST_CHECK_EQUAL(response.reason_phrase, "OK");
+            }
+
+            BOOST_AUTO_TEST_CASE(ok_rtsp25_19_repsonse_test) {
+                parse_phrase(ok_rtsp25_19_repsonse);
+                BOOST_CHECK(success);
+                BOOST_CHECK(begin == end);
+                BOOST_CHECK_EQUAL(response.rtsp_version_major, 25);
+                BOOST_CHECK_EQUAL(response.rtsp_version_minor, 19);
+                BOOST_CHECK_EQUAL(response.status_code, 200);
                 BOOST_CHECK_EQUAL(response.reason_phrase, "OK");
             }
 
@@ -75,9 +90,9 @@ BOOST_AUTO_TEST_SUITE(rtsp)
                 parse_phrase(ok_rtsp1_1_repsonse);
                 BOOST_CHECK(success);
                 BOOST_CHECK(begin == end);
-                BOOST_CHECK_EQUAL(response.rtsp_version_major, "1");
-                BOOST_CHECK_EQUAL(response.rtsp_version_minor, "1");
-                BOOST_CHECK_EQUAL(response.status_code, "200");
+                BOOST_CHECK_EQUAL(response.rtsp_version_major, 1);
+                BOOST_CHECK_EQUAL(response.rtsp_version_minor, 1);
+                BOOST_CHECK_EQUAL(response.status_code, 200);
                 BOOST_CHECK_EQUAL(response.reason_phrase, "OK");
             }
 
