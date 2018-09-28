b68da1436aaff7a990e348722ced6360039cab64
Markus Klemm <markus@markus@markusklemm.net>
2018-09-28 11:12:26 +0200

    Project cleanup: removed unused externalized boost_network adapter

--- src/boost_network_adapter/src/boost_network_adapter.cpp
+++
@@ -1,8 +0,0 @@
-/*! @file boost_network_adapter.cpp
- *
- */
-#include <boost_network_adapter.hpp>
-
-int lib::foo() {
-    return 5;
-}
--- test/general_tests.cpp
+++ test/general_tests.cpp
@@ -2,14 +2,10 @@
 
 #include <boost/test/included/unit_test.hpp>
 
-#include <boost_network_adapter.hpp>
-#include <rtsp_request.hpp>
 
 BOOST_AUTO_TEST_SUITE(test_suite)
 
     BOOST_AUTO_TEST_CASE(FIRST_TEST) {
-        lib a;
-        BOOST_CHECK_EQUAL(a.foo(), 5);
         BOOST_CHECK(true);
     }
 
