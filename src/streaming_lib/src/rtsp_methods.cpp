18fb4b4a353cdea796845b789b69d6b5f981dd9f
Markus Klemm <markus@markus@markusklemm.net>
2018-10-28 13:57:09 +0100

    Some first transport header touching in SETUP

--- src/streaming_lib/src/rtsp_methods.cpp
+++ src/streaming_lib/src/rtsp_methods.cpp
@@ -17,6 +17,30 @@ std::pair<rtsp::response, rtsp::body> rtsp::methods::setup(rtsp::rtsp_session &s
                                                            const rtsp::internal_request &request) {
     rtsp::response response{common_response_sekeleton(session, request)};
 
+    rtsp::headers::transport request_transport{};
+
+
+    if (request.second.count("transport")) {
+        rtsp::headers::transport_grammar<std::string::const_iterator> transport_grammar{};
+        auto begin = request.second.at("transport").cbegin();
+        auto end = request.second.at("transport").cend();
+
+        boost::spirit::qi::phrase_parse(begin, end, transport_grammar, boost::spirit::ascii::space, request_transport);
+
+        if (begin != end) {
+            response.status_code = 400;
+            response.reason_phrase = rtsp::string{"Bad Request: Could not read transport header after"} +
+                                     rtsp::string{begin, end};
+            return std::make_pair<rtsp::response, rtsp::body>(std::move(response), {});
+        }
+    }
+
+    rtsp::headers::transport response_transport{};
+
+    rtsp::string transport_string;
+    rtsp::headers::generate_transport_header_grammar<std::back_insert_iterator<std::string>> transport_gen_grammar{};
+    boost::spirit::karma::generate(std::back_inserter(transport_string), transport_gen_grammar, response_transport);
+    response.headers.emplace_back("Transport", std::move(transport_string));
 
     return std::make_pair<rtsp::response, rtsp::body>(std::move(response), {});
 }
