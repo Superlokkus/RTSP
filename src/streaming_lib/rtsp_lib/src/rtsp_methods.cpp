c4f2181eee92e61bee30d85debfa5252e0478ff3
Markus Klemm <markus@markus@markusklemm.net>
2018-11-24 16:38:32 +0100

    Looks like VLC doesn't like just port numbers

--- src/streaming_lib/rtsp_lib/src/rtsp_methods.cpp
+++ src/streaming_lib/rtsp_lib/src/rtsp_methods.cpp
@@ -1,5 +1,8 @@
 #include <rtsp_methods.hpp>
 
+#include <algorithm>
+#include <random>
+
 #include <rtsp_headers.hpp>
 
 #include <boost/uuid/uuid_io.hpp>
@@ -11,6 +14,92 @@ rtsp::response rtsp::methods::common_response_sekeleton(const rtsp::rtsp_session
                                   {"CSeq", request.second.at("cseq")},
                                   {"Session", boost::uuids::to_string(session.identifier())},
                           }};
+}
+
+auto get_acceptable_transport(const rtsp::headers::transport &requested)
+-> boost::optional<rtsp::headers::transport::transport_spec> {
+    thread_local std::default_random_engine re{};
+    using namespace rtsp;
+    boost::optional<rtsp::headers::transport::transport_spec> choosen{};
+
+    auto requested_transport_spec = std::find_if(requested.specifications.cbegin(),
+                                                 requested.specifications.cend(),
+                                                 [](const headers::transport::transport_spec &value) {
+                                                     return value.transport_protocol == "RTP" &&
+                                                            value.profile == "AVP" &&
+                                                            value.lower_transport.value_or("UDP") == "UDP" &&
+                                                            value.parameters.size() > 0 &&
+                                                            value.parameters.at(0).which() == 4 &&
+                                                            boost::get<string>(value.parameters.at(0)) == "unicast";
+                                                 });
+    if (requested_transport_spec == requested.specifications.cend())
+        return choosen;
+
+    rtsp::headers::transport::transport_spec choosen_spec{"RTP", "AVP", boost::make_optional<string>("UDP")};
+
+    auto parameter_begin = requested_transport_spec->parameters.cbegin();
+    auto parameter_end = requested_transport_spec->parameters.cend();
+
+    choosen_spec.parameters.push_back(rtsp::string{"unicast"});
+
+    auto parameter = std::find_if(parameter_begin, parameter_end,
+                                  [](const auto &value) {
+                                      return value.which() == 1 &&
+                                             boost::get<headers::transport::transport_spec::port>(value).type ==
+                                             headers::transport::transport_spec::port::port_type::client;
+                                  });
+    if (parameter == parameter_end) {
+        std::uniform_int_distribution<headers::transport::transport_spec::port_number> ud{49152u, 65525u};
+        headers::transport::transport_spec::port client_port{
+                headers::transport::transport_spec::port::port_type::client,
+                ud(re)};
+        choosen_spec.parameters.push_back(client_port);
+    } else {
+        const auto &suggest_port = boost::get<headers::transport::transport_spec::port>(*parameter);
+        headers::transport::transport_spec::port client_port{
+                headers::transport::transport_spec::port::port_type::client,
+                headers::transport::transport_spec::port_number{}};
+        if (suggest_port.port_numbers.which() == 0) {
+            client_port.port_numbers = suggest_port.port_numbers;
+        } else {
+            client_port.port_numbers = boost::get<headers::transport::transport_spec::port_range>
+                    (suggest_port.port_numbers).first;
+        }
+
+        choosen_spec.parameters.push_back(client_port);
+    }
+
+
+    parameter = std::find_if(parameter_begin, parameter_end,
+                             [](const auto &value) {
+                                 return value.which() == 2 &&
+                                        boost::get<headers::transport::transport_spec::ssrc>(value) != 0u;
+                             });
+    if (parameter == parameter_end) {
+        std::uniform_int_distribution<headers::transport::transport_spec::ssrc> ud{};
+        choosen_spec.parameters.push_back(headers::transport::transport_spec::ssrc{ud(re)});
+    } else {
+        choosen_spec.parameters.push_back(*parameter);
+    }
+
+    parameter = std::find_if(parameter_begin, parameter_end,
+                             [](const auto &value) {
+                                 return value.which() == 1 &&
+                                        boost::get<headers::transport::transport_spec::port>(value).type ==
+                                        headers::transport::transport_spec::port::port_type::server;
+                             });
+    if (parameter == parameter_end) {
+        const auto &client_port = boost::get<headers::transport::transport_spec::port>(choosen_spec.parameters.at(1));
+        headers::transport::transport_spec::port server_port{
+                headers::transport::transport_spec::port::port_type::server,
+                boost::get<headers::transport::transport_spec::port_number>(client_port.port_numbers) + 2};
+        choosen_spec.parameters.push_back(server_port);
+    } else {
+        choosen_spec.parameters.push_back(*parameter);
+    }
+
+
+    return choosen = choosen_spec;
 }
 
 std::pair<rtsp::response, rtsp::body> rtsp::methods::setup(rtsp::rtsp_session &session,
@@ -35,7 +124,16 @@ std::pair<rtsp::response, rtsp::body> rtsp::methods::setup(rtsp::rtsp_session &s
         }
     }
 
+    auto choosen_transport = get_acceptable_transport(request_transport);
+    if (!choosen_transport) {
+        response.status_code = 461;
+        response.reason_phrase = rtsp::string{"Unsupported Transport: The Transport field did not contain a "
+                                              "supported transport specification."};
+        return std::make_pair<rtsp::response, rtsp::body>(std::move(response), {});
+    }
+
     rtsp::headers::transport response_transport{};
+    response_transport.specifications.push_back(choosen_transport.value());
 
     rtsp::string transport_string;
     rtsp::headers::generate_transport_header_grammar<std::back_insert_iterator<std::string>> transport_gen_grammar{};
@@ -44,6 +142,7 @@ std::pair<rtsp::response, rtsp::body> rtsp::methods::setup(rtsp::rtsp_session &s
 
     return std::make_pair<rtsp::response, rtsp::body>(std::move(response), {});
 }
+
 
 std::pair<rtsp::response, rtsp::body> rtsp::methods::teardown(rtsp::rtsp_session &session,
                                                               const rtsp::internal_request &request) {
